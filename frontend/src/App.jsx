62ae258da9faebdbf13aad928932ad04d811317e
nizam <nizampnizam7@gmail.com>
2025-12-03 17:17:15 +0530

    fix register import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import React, { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
 import Chat from "./pages/Chat.jsx";
-
+import Register from "./pages/Register.jsx"
 export default function App() {
   const { user } = useContext(AuthContext);
   return (
